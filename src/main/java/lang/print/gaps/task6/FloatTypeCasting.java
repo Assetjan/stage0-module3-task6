fce0fcfdd88e4215c6e5ec6ac369fca8afa05e9f
Assetjan <asetziyabek@gmail.com>
2023-01-15 19:10:42 +0600

    Task solved

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,7 +2,14 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-        int cba = (int)numberToBeRounded;
+        int cba;
+        if((numberToBeRounded*10)%10<5) {
+            cba = (int) numberToBeRounded;
+        }
+        else {
+            cba = (int) numberToBeRounded;
+            cba+1;
+        }
         System.out.println(cba);
     }
 }
