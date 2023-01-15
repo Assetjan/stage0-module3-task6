548d919667834986ef0c282937df8d5b9c6e0998
Assetjan <asetziyabek@gmail.com>
2023-01-15 18:57:36 +0600

    Task solved

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long abc = 1000L;
+        int cba = (int)abc;
+        System.out.println(abc);
+        System.out.println(cba);
     }
 }
